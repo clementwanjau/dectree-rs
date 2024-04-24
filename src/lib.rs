aacff35d69da51997929eebabe196ce8bea02a20
clement <clementwanjau@gmail.com>
2024-04-24 04:43:50 +0300

    Add some documentation for the crate.

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-include_str!("../README.md");
+#[doc = include_str!("../readme.md")]
 
 use std::collections::HashMap;
 
@@ -174,5 +174,36 @@ impl<'a, T> SignatureDecisionTree<'a, T> where T: Clone + Default {
 			matches.sort_by(|a, b| a.bytes.len().cmp(&b.bytes.len()));
 			matches.first().map(|x| x.object.clone())
 		}
+	}
+}
+
+#[cfg(test)]
+mod tests {
+	#[test]
+	fn test_signature_subset() {
+		// signature_base = b'\x55\xe9\xd8\x01\xfe\xff\x32\x77\x89\x4f\x55'
+		//         sigtree = envi.bytesig.SignatureTree()
+		//         sigtree.addSignature(signature_base)
+		//
+		//         sigtree.getSignature(b'\x55\xe9')
+		//         sigtree.addSignature(signature_base[:7], val=signature_base[:7])
+		//         sigtree.addSignature(signature_base[:4], val=signature_base[:4])
+		//         sigtree.addSignature(signature_base + b'\xfe\x38', val=signature_base + b'\xfe\x38')
+		//
+		//         self.assertTrue(sigtree.getSignature(b'\x55\xe9\xd8\x01\xfe\xff\x32\x00\x99\x36\x5f\x21\xfd') == signature_base[:7])
+		//         self.assertTrue(sigtree.getSignature(b'\x55\xe9\xd8\x01\xfe\xff\x32') == signature_base[:7])
+		//         self.assertTrue(sigtree.getSignature(b'\x55\xe9\xd8\x01\xfe\x00') == signature_base[:4])
+		//         self.assertTrue(sigtree.getSignature(b'\x55') is None)
+		let signature_base = vec![0x55, 0xe9, 0xd8, 0x01, 0xfe, 0xff, 0x32, 0x77, 0x89, 0x4f, 0x55];
+		let mut tree = super::SignatureDecisionTree::new();
+		tree.add_signature(signature_base.clone(), None, None);
+		tree.get_signature(vec![0x55, 0xe9], None);
+		tree.add_signature(signature_base.clone().into_iter().take(7).collect(), None, Some(signature_base.clone().into_iter().take(7).collect()));
+		tree.add_signature(signature_base.clone().into_iter().take(4).collect(), None, Some(signature_base.clone().into_iter().take(4).collect()));
+		tree.add_signature([signature_base.clone(), vec![0xfe, 0x38]].concat(), None, Some([signature_base.clone(), vec![0xfe, 0x38]].concat()));
+		assert_eq!(tree.get_signature(vec![0x55, 0xe9, 0xd8, 0x01, 0xfe, 0xff, 0x32, 0x00, 0x99, 0x36, 0x5f, 0x21, 0xfd], None), Some(signature_base.clone().into_iter().take(7).collect()));
+		assert_eq!(tree.get_signature(vec![0x55, 0xe9, 0xd8, 0x01, 0xfe, 0xff, 0x32], None), Some(signature_base.clone().into_iter().take(7).collect()));
+		assert_eq!(tree.get_signature(vec![0x55, 0xe9, 0xd8, 0x01, 0xfe, 0x00], None), Some(signature_base.clone().into_iter().take(4).collect()));
+		assert_eq!(tree.get_signature(vec![0x55], None), None);
 	}
 }
